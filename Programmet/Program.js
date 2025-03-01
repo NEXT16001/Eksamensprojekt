1f233a469c25d3dcac54b7a18ce3423dff63995c
NEXT16001 <NEXT16001@edu.nextkbh.dk>
2025-03-01 21:59:20 +0100

    Third

--- Programmet/Program.js
+++ Programmet/Program.js
@@ -3,6 +3,8 @@ function setup() {
   let knapForside = createButton('Forsiden');
   knapForside.position(1,1);
   knapForside.mousePressed(gåTilbageTilForsiden);
+
+  inputPeriodeOgSum();
   
 }
 
@@ -15,6 +17,15 @@ function gåTilbageTilForsiden() {
   window.location.href = 'index.html';
 }
 
-function inputPeriode&Sum() {
-  
+function inputPeriodeOgSum() {
+  let inputY = windowHeight/8;
+  text("Hvor meget vil du sparer?",windowWidth/7, inputY);
+  let sumInput = createInput();
+  sumInput.position(windowWidth/4+20, inputY);
+  text("kr.", windowWidth/3+80, inputY);
+
+  text("Hvor længe vil du sparer?", windowWidth*5/10, inputY);
+  let periodeInput = createInput();
+  periodeInput.position(windowWidth*6.25/10, inputY);
+  text("måneder", windowWidth*7/10+75, inputY);
 }
