4c965a065031c8daf317bba732bc885c89d6a8b4
NEXT16001 <NEXT16001@edu.nextkbh.dk>
2025-03-04 20:12:25 +0100

    Thrid

--- Programmet/Program.js
+++ Programmet/Program.js
@@ -1,3 +1,7 @@
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+let inputY = windowHeight/8;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   let knapForside = createButton('Forsiden');
@@ -5,11 +9,14 @@ function setup() {
   knapForside.mousePressed(gåTilbageTilForsiden);
 
   inputPeriodeOgSum();
+  låsPeriodeOgSum();
   
 }
 
 function draw() {
   background(255);
+  
+  inputPeriodeOgSumText();
 
 }
 
@@ -18,14 +25,27 @@ function gåTilbageTilForsiden() {
 }
 
 function inputPeriodeOgSum() {
-  let inputY = windowHeight/8;
-  text("Hvor meget vil du sparer?",windowWidth/7, inputY);
   let sumInput = createInput();
   sumInput.position(windowWidth/4+20, inputY);
+
+  let periodeInput = createInput();
+  periodeInput.position(windowWidth*6.25/10, inputY);
+}
+
+function inputPeriodeOgSumText() {
+  text("Hvor meget vil du sparer?",windowWidth/7, inputY);
   text("kr.", windowWidth/3+80, inputY);
 
   text("Hvor længe vil du sparer?", windowWidth*5/10, inputY);
-  let periodeInput = createInput();
-  periodeInput.position(windowWidth*6.25/10, inputY);
   text("måneder", windowWidth*7/10+75, inputY);
+}
+
+function låsPeriodeOgSum(inputPeriodeOgSum) {
+  let låsPeriodeOgSumKnap = createButton("Ok");
+  låsPeriodeOgSumKnap.position(windowWidth*8/10, inputY);
+  
+  if (mousePressed){
+    sumInput.attribute("remove");
+    periodeInput.attribute("remove"); 
+  }
 }
