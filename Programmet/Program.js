c48db8e769cbb73ec029e217ba338403ac1b8752
NEXT16001 <NEXT16001@edu.nextkbh.dk>
2025-03-12 09:44:44 +0100

    Fourth

--- Programmet/Program.js
+++ Programmet/Program.js
@@ -33,19 +33,23 @@ function inputPeriodeOgSum() {
 }
 
 function inputPeriodeOgSumText() {
-  text("Hvor meget vil du sparer?",windowWidth/7, inputY);
-  text("kr.", windowWidth/3+80, inputY);
+  let list = ["Hvor meget vil du sparer?", "kr.", "Hvor længe vil du sparer?", "måneder"];
+  for (let i = 0; i < 4; i++) {
+    text(list[0],windowWidth/7, inputY);
+    text(list[1], windowWidth/3+80, inputY);
 
-  text("Hvor længe vil du sparer?", windowWidth*5/10, inputY);
-  text("måneder", windowWidth*7/10+75, inputY);
+    text(list[2], windowWidth*5/10, inputY);
+    text(list[3], windowWidth*7/10+75, inputY);
+  }
 }
 
 function låsPeriodeOgSum(inputPeriodeOgSum) {
   let låsPeriodeOgSumKnap = createButton("Ok");
-  låsPeriodeOgSumKnap.position(windowWidth*8/10, inputY);
-  
-  if (mousePressed){
-    sumInput.attribute("remove");
-    periodeInput.attribute("remove"); 
-  }
+  låsPeriodeOgSumKnap.position(windowWidth*9/10-100, inputY);
+  låsPeriodeOgSumKnap.mousePressed(låsPeriodeOgSumAttribute);
+}
+
+function låsPeriodeOgSumAttribute (låsPeriodeOgSum) {
+  sumInput.attribute(disabled);
+  periodeInput.attribute(disabled);
 }
